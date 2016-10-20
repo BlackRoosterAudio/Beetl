3cae6b5974b8aff72b8b2e60c3e766fe244f7b26
André Kirchner <andre@andrekirchner.com>
2016-10-20 15:07:49 +0200

    Minor update

--- frontend/app/scripts/controllers/catalogue.list.js
+++ frontend/app/scripts/controllers/catalogue.list.js
@@ -10,7 +10,8 @@
 angular.module('beetlApp').controller('CatalogueListCtrl', ['$scope', '$rootScope', 'apiHandler', '$state', function($scope, $rootScope, apiHandler, $state) {
 
 	// Load helpers
-	$scope.helpers = $rootScope.helpers;
+	$scope.helpers     = $rootScope.helpers;
+	$scope.currentUser = $rootScope.currentUser;
 
 	// Prequisites
 	$scope.projects = {};
@@ -26,6 +27,36 @@ angular.module('beetlApp').controller('CatalogueListCtrl', ['$scope', '$rootScop
 			$state.go('errorPage', { errorId: 2, msg: err });
 		});
 
+	var enrichCatalogueList = function() {
+		if ($scope.catalogues.length > 0 && $scope.results.length > 0) {
+			for (var i = 0; i < $scope.catalogues.length; i++) {
+				$scope.catalogues[i].alreadyDone = false;
+				for (var j = 0; j < $scope.results.length; j++) {
+					if ($scope.currentUser.id == $scope.results[j].testedBy && $scope.results[j].catalogue == $scope.catalogues[i]._id) {
+						$scope.catalogues[i].alreadyDone = true;
+						console.log('found in', $scope.catalogues[i]);
+					}
+				}
+			}
+		}
+	};
+
+	/**
+	 * The function will initialize the result list
+	 */
+	var initializeResultList = function() {
+		$scope.results = [];
+		apiHandler.getResultList()
+			.success(function(res) {
+				$scope.results = res;
+				
+				enrichCatalogueList();
+			})
+			.error(function(err) {
+				$state.go('errorPage', { errorId: 1, msg: err });
+			});
+	};
+
 	/**
 	 * The function will initialize the catalogue list
 	 */
@@ -34,6 +65,8 @@ angular.module('beetlApp').controller('CatalogueListCtrl', ['$scope', '$rootScop
 		apiHandler.getCatalogueList()
 			.success(function(res) {
 				$scope.catalogues = res;
+
+				initializeResultList();
 			})
 			.error(function(err) {
 				$state.go('errorPage', { errorId: 1, msg: err });
